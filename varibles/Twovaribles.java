b2566426294cbd4eb195cb8ab225409872330f13
abhilashputhukkudi <abhilashp44@gmail.com>
2021-05-04 13:37:20 +0530

    completed

---
+++ varibles/Twovaribles.java
@@ -0,0 +1,36 @@
+class Twovaribles{
+
+public static void main(String args[]) {
+
+ int num1=100,num2=50;// declaring int varibles and assigning values
+ 
+                        //printing values
+ 
+  System.out.println("VALUE OF NUM1 : "+num1);
+  System.out.println("VALUE OF NUM2 : "+num2);
+
+  System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+  System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
+  System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
+  System.out.println(num1 + " / " + num2 + " = " + (num1/num2));	
+
+}// end of main
+
+}// end of class
+
+
+
+/*
+   OUTPUT
+   ------
+   ablas@ablas-HP-Notebook:~/Desktop/JAVA PROGRAMS/varibles$ javac Twovaribles.java
+   ablas@ablas-HP-Notebook:~/Desktop/JAVA PROGRAMS/varibles$ java Twovaribles
+   VALUE OF NUM1 : 100
+   VALUE OF NUM2 : 50
+   100 + 50 = 150
+   100 - 50 = 50
+   100 * 50 = 5000
+   100 / 50 = 2
+
+ 
+*/
---
+++ varibles/Varibles.java
@@ -0,0 +1,14 @@
+class Varibles{
+
+public static void main(String args[]) {
+
+ int num1;// declaring int varible
+ num=100;//assiging value
+ 
+ System.out.print("VALUE OF NUM : "+num);
+
+ System.out.println("VALUE OF NUM*2="+num*2);
+	
+}// end of main
+
+}// end of class
