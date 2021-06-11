a54c6e7c54b4bf198eb2b6fabd165c432862b138
abhilashputhukkudi <abhilashp44@gmail.com>
2021-06-11 15:03:23 +0530

    string sort uploaded

--- EXCERCISE 3/3/StringSort.java
+++ EXCERCISE 3/3/StringSort.java
@@ -56,12 +56,18 @@ class StringSort{
       Sort sort = new Sort(); //sort class object
       Scanner in = new Scanner(System.in);   
     
-     
-     while(true)
+      int option;//option varible
+
+        //displaying menu
+          System.out.println("\nCHOOSE YOUR OPTION\n----------------- ");
+          System.out.println("1. BUILT IN METHOD \n2. USER DEFINED METHOD\n3.EXIT\n");
+          System.out.print("ENTER YOUR OPTION : ");
+          option=in.nextInt(); //rading option varible
+     while(option!=3)
      {
 
 
-          System.out.print("ENTER NUMBER OF WORDS : ");
+          System.out.print("\nENTER NUMBER OF WORDS : ");
             int noOfWords = in.nextInt();
 
 
@@ -85,29 +91,113 @@ class StringSort{
                  System.out.println(StringArray[i]);
              } 
       
-      int option;//option varible
+            switch(option)
+            {  //switching option
+                case 1: sort.BuiltInSorting(StringArray,noOfWords); 
+                    break;
+                case 2: sort.UserDefinedSorting(StringArray,noOfWords);
+                    break;
+                default: System.out.println("\n INVALID OPTION "); 
+
+            }
           //displaying menu
           System.out.println("\nCHOOSE YOUR OPTION\n----------------- ");
           System.out.println("1. BUILT IN METHOD \n2. USER DEFINED METHOD\n3.EXIT\n");
           System.out.print("ENTER YOUR OPTION : ");
           option=in.nextInt(); //rading option varible
           
-           switch(option){//switching option
-            case 1: sort.BuiltInSorting(StringArray,noOfWords); 
-                    break;
-            case 2: sort.UserDefinedSorting(StringArray,noOfWords);
-                    break;
-            case 3: break;
-            default: System.out.println("\n INVALID OPTION "); 
-
-            }
+          
      }
 
-   
+   System.out.println("EXITING...");
       
  }
 }
 
+/*
 
+OUTPUT
+------
+ablas@ablas-HP-Notebook:~/Desktop/JAVA PROGRAMS/EXCERCISE 3/3$ javac StringSort.java
+ablas@ablas-HP-Notebook:~/Desktop/JAVA PROGRAMS/EXCERCISE 3/3$ java StringSort
+
+CHOOSE YOUR OPTION
+----------------- 
+1. BUILT IN METHOD 
+2. USER DEFINED METHOD
+3.EXIT
+
+ENTER YOUR OPTION : 1
+
+ENTER NUMBER OF WORDS : 5
+ENTER WORDS 
+1) apple
+2) juice
+3) cat
+4) ball
+5) zeebra
+ENTERD WORDS : 
+1) apple
+2) juice
+3) cat
+4) ball
+5) zeebra
+
+SORTED ARRAY 
+1) apple
+2) ball
+3) cat
+4) juice
+5) zeebra
+
+CHOOSE YOUR OPTION
+----------------- 
+1. BUILT IN METHOD 
+2. USER DEFINED METHOD
+3.EXIT
+
+ENTER YOUR OPTION : 3
+ablas@ablas-HP-Notebook:~/Desktop/JAVA PROGRAMS/EXCERCISE 3/3$ javac StringSort.java
+ablas@ablas-HP-Notebook:~/Desktop/JAVA PROGRAMS/EXCERCISE 3/3$ java StringSort
+
+CHOOSE YOUR OPTION
+----------------- 
+1. BUILT IN METHOD 
+2. USER DEFINED METHOD
+3.EXIT
+
+ENTER YOUR OPTION : 2
+
+ENTER NUMBER OF WORDS : 4
+ENTER WORDS 
+1) apple
+2) dog
+3) cat
+4) orange 
+ENTERD WORDS : 
+1) apple
+2) dog
+3) cat
+4) orange
+
+SORTED ARRAY 
+1) apple
+2) cat
+3) dog
+4) orange
+
+CHOOSE YOUR OPTION
+----------------- 
+1. BUILT IN METHOD 
+2. USER DEFINED METHOD
+3.EXIT
+
+ENTER YOUR OPTION : 3
+
+EXITING...
+
+
+
+*/
 
 
