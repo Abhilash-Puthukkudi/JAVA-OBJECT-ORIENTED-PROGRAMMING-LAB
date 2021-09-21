070a7080a0f2ea968d6232676b7354fdca1cc8e1
Abhilash.P <abhilashp44@gmail.com>
2021-09-21 16:03:39 +0530

    10 COMPLETED

---
+++ EXERCISE 10/6/HomeDraw.java
@@ -0,0 +1,102 @@
+/*
+6. Using 2D graphics commands in an Applet, construct a house.
+On mouse click event, change the color of the door from blue to
+red.
+*/
+
+
+
+import java.awt.*;
+import java.awt.event.*;
+
+ class HomeDraw extends Frame implements MouseListener, MouseMotionListener {
+
+	
+	
+       Color color = Color.BLUE;	
+      	public HomeDraw() {
+		addMouseListener(this);
+		addMouseMotionListener(this);
+        addWindowListener(new WindowAdapter() {
+		public void windowClosing(WindowEvent we) {
+			System.exit(0);
+		  }
+	     });
+       
+       
+		
+	}
+										       
+    // Handle mouse clicked.
+	public void mouseClicked(MouseEvent me) {
+       if(color==Color.BLUE)  //switching colors
+         color =Color.RED;	
+       else
+         color=Color.BLUE;						
+		repaint();
+	}
+
+									         
+	public void mouseEntered(MouseEvent me) {
+	  // Handle mouse entered.
+	}
+
+									
+	public void mouseExited(MouseEvent me) {
+		// Handle mouse exited.
+		
+	}
+
+			                        
+	public void mousePressed(MouseEvent me) {
+		// Handle button pressed.
+	}
+
+						     		
+	public void mouseReleased(MouseEvent me) {
+		// Handle button released.
+	}
+	
+									
+	public void mouseDragged(MouseEvent me) {
+		// Handle mouse dragged.
+
+	}
+
+								   
+	public void mouseMoved(MouseEvent me) {
+		// Handle mouse moved.
+	}
+					
+	public void paint(Graphics g) {
+		
+            g.setColor(Color.LIGHT_GRAY);
+			g.fillRect(240, 200, 250, 250); //walls
+
+	        g.setColor(Color.YELLOW); //WINDOW1
+            g.fillRect(260, 240, 50, 50);
+
+            g.setColor(Color.YELLOW); //WINDOW2
+            g.fillRect(420, 240, 50, 50);
+
+			g.setColor(color);				
+  			g.fillRect(330, 300, 70, 140); //door
+			                               
+			g.setColor(Color.DARK_GRAY);  // roof
+			int x[]={350,190,540};
+			int y[]={80,200,200};
+			g.fillPolygon(x,y,3);
+	}
+
+
+	public static void main(String[] args) {
+		HomeDraw Homeapp = new  HomeDraw();
+
+		Homeapp.setSize(new Dimension(700,500));
+		Homeapp.setTitle("HOME");
+		Homeapp.setVisible(true);
+	}
+}
+
+
+
---
+++ EXERCISE 10/7/MouseWindow.java
@@ -0,0 +1,121 @@
+/*
+7. Develop a program to handle all mouse events and window events
+8*/
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class MouseWindow extends Frame implements MouseListener, MouseMotionListener,WindowListener {
+
+String msg,Wmsg="WINDOW EVENTS : ";
+int mouseX = 0, mouseY = 0; // coordinates of mouse at first
+
+public MouseWindow() {
+    addWindowListener(this);  
+    addMouseListener(this);
+    addMouseMotionListener(this);
+    
+}
+
+
+ //window activated
+public void windowActivated(WindowEvent arg0) {  
+    Wmsg= Wmsg+ " activated ";  
+    repaint();
+}  //window closed
+public void windowClosed(WindowEvent arg0) {  
+     Wmsg= Wmsg+ " closed ";  
+    repaint();
+}  // window closing
+public void windowClosing(WindowEvent arg0) {  
+     Wmsg= Wmsg+ " closing ";  
+    dispose();  
+    repaint();
+}  
+//window
+public void windowDeactivated(WindowEvent arg0) {  
+    Wmsg= Wmsg+ " deactivated "; 
+   repaint(); 
+}  
+public void windowDeiconified(WindowEvent arg0) {  
+    Wmsg= Wmsg+ " deiconified "; 
+    repaint(); 
+}  
+public void windowIconified(WindowEvent arg0) {  
+     Wmsg= Wmsg+ " iconified ";  
+    repaint();
+}  
+public void windowOpened(WindowEvent arg0) {  
+    Wmsg= Wmsg+ " opened ";  
+    repaint();
+}  
+
+
+
+
+
+// Handle mouse clicked.
+public void mouseClicked(MouseEvent me) {
+msg = msg + " click received";
+repaint();
+}
+// Handle mouse entered.
+public void mouseEntered(MouseEvent me) {
+mouseX = 100;
+mouseY = 100;
+msg = "Mouse entered.";
+repaint();
+}
+// Handle mouse exited.
+public void mouseExited(MouseEvent me) {
+mouseX = 100;
+mouseY = 100;
+msg = "Mouse exited.";
+repaint();
+}
+
+// Handle button pressed.
+public void mousePressed(MouseEvent me) {
+// save coordinates
+mouseX = me.getX();
+mouseY = me.getY();
+msg = "Button down";
+repaint();
+}
+// Handle button released.
+public void mouseReleased(MouseEvent me) {
+// save coordinates
+mouseX = me.getX();
+mouseY = me.getY();
+msg = "Button Released";
+repaint();
+}
+// Handle mouse dragged.
+public void mouseDragged(MouseEvent me) {
+// save coordinates
+mouseX = me.getX();
+mouseY = me.getY();
+msg = "*" + " mouse at " + mouseX + ", " + mouseY;
+repaint();
+}
+// Handle mouse moved.
+public void mouseMoved(MouseEvent
+me) {
+msg = "Moving mouse at " + me.getX()
++ ", " + me.getY();
+repaint();
+}
+// Display msg in the window at current X,Y location.
+public void paint(Graphics g) {
+g.drawString(Wmsg,20, 90);
+g.drawString(msg, mouseX, mouseY);
+}
+public static void main(String[] args) {
+MouseWindow windowmosue = new MouseWindow();
+
+windowmosue.setSize(new Dimension(300, 300));
+windowmosue.setTitle("Mouse&Window Events");
+windowmosue.setVisible(true);
+}
+}
+
