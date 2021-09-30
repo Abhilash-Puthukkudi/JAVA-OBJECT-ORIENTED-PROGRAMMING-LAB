a8091359b035b51f983060ab45755b4cc0d58e77
Abhilash-Puthukkudi <abhilashp44@gmail.com>
2021-09-30 19:32:14 +0530

    updated odd or even program to support 2 or more  digit

--- EXERCISE 11/4/Oddeven.java
+++ EXERCISE 11/4/Oddeven.java
@@ -22,41 +22,83 @@ try(
     FileOutputStream foutOdd = new FileOutputStream("ODD.txt");
     FileOutputStream foutEven = new FileOutputStream("EVEN.txt");
     ) {
-               
+            String svalue ="";
             do {
                 content = fin.read();
-                if(content != -1){
+                if(content != -1){ 
+                    if((char)content != '\n'){ //checking nextline character
+                    char value =(char)content; // reading each character
+                    svalue =svalue+ String.valueOf(value); // concatinating if its a two or more digit number
                     
-                    char value =(char)content;
-                    String svalue = String.valueOf(value);
-                    int ivalue =Integer.parseInt(svalue);
-                 
-                    if(ivalue%2==0){
-                      foutEven.write(content);
-                    }
-                    else{
-                      foutOdd.write(content);
-                    }
+                    }else if((char)content=='\n'){ // next line
+                      
+                         int ivalue =Integer.parseInt(svalue); // converting number to integer
+                         svalue=svalue+"\n"; // adding nextline character to number string
+                                       if(ivalue%2==0){ //cheking even or odd
+                                             byte bE[] = svalue.getBytes(); // writing to file
+                                             foutEven.write(bE);
+                                        }
+                                        else{
+                                             byte bO[] = svalue.getBytes();// writing to file
+                                             foutOdd.write(bO);
+                                        }
+                      svalue=""; // clearing the number string
+                     }
                 }
+                 
                 } while(content != -1);
-                
+                System.out.println(" 'ODD.txt' and 'EVEN.txt' created and numbers copyed successfully!..");
            } 
            catch(FileNotFoundException e) {
                     System.out.println("File Not Found");
                     return;
-             } catch(Exception e) {System.out.println(" 'ODD.txt' and 'EVEN.txt' created and numbers copyed successfully!..");}
+             } catch(Exception e) {}
 
 }}
 
-
-
 /*
-output
--------
+OUTPUT
+------
+
 abhilashp@ablas:~/Desktop/Class Labs/JAVA-OBJECT-ORIENTED-PROGRAMMING-LAB/EXERCISE 11/4$ javac Oddeven.java
 abhilashp@ablas:~/Desktop/Class Labs/JAVA-OBJECT-ORIENTED-PROGRAMMING-LAB/EXERCISE 11/4$ java Oddeven
 Enter input file : num.txt
- 'ODD.txt' and 'EVEN.txt' created and numbers copyed successfully!..
+'ODD.txt' and 'EVEN.txt' created and numbers copyed successfully!..
+
+
+abhilashp@ablas:~/Desktop/Class Labs/JAVA-OBJECT-ORIENTED-PROGRAMMING-LAB/EXERCISE 11/4$ cat num.txt
+10
+21
+31
+4
+5
+6
+7
+8
+9
+
+
+abhilashp@ablas:~/Desktop/Class Labs/JAVA-OBJECT-ORIENTED-PROGRAMMING-LAB/EXERCISE 11/4$ cat EVEN.txt
+10
+4
+6
+8
+
+
+abhilashp@ablas:~/Desktop/Class Labs/JAVA-OBJECT-ORIENTED-PROGRAMMING-LAB/EXERCISE 11/4$ cat ODD.txt
+21
+31
+5
+7
+9
+
+
 
 
 */
+
+
+
+
+
+
